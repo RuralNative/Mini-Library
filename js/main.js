0a3bedc4579440002376ee3205966587911b90a4
RuralNative <johnberlinleonor2001@gmail.com>
2024-11-18 04:07:23 -0800

    Added Code for Book() Constructor

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const libraryStorage = [];
+
+function Book(title, author, pages, read_status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read_status = read_status;
+}
+
+function addBook() {
+    // Fetch input from HTML form
+    // Use input for Book() arguments (title, author, pages, read)
+    // Create Book object with Book()
+    // Store new Book object within libraryStorage[]
+};
