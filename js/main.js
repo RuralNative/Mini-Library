dcef40de61675b8ca50dd8a56a7eaa067ffe6aa4
RuralNative <johnberlinleonor@yahoo.com>
2024-11-18 10:29:32 -0800

    Create Init HTML Code

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Array to store all Book objects
+const library_storage = [];
+
+function Book(title, author, number_of_pages, read_status) {
+    this.title = title;
+    this.author = author;
+    this.number_of_pages = number_of_pages;
+    this.read_status = read_status;
+}
+
+function addBook() {
+    // Fetch info from HTML Form
+    // Use info for Book() arguments and create a Book() object
+    // Store new Book object in library_storage
+}
